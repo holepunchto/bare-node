c3b511993d1463b0c12f868289b1f38c5d979dcb
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-09-18 09:54:44 +0200

    Move out `bare-node-runtime`

--- npm/runtime/global.js
+++
@@ -1,7 +0,0 @@
-require('bare-abort-controller/global')
-require('bare-crypto/global')
-require('bare-encoding/global')
-require('bare-events/global')
-require('bare-fetch/global')
-require('bare-process/global')
-require('bare-stream/global')
